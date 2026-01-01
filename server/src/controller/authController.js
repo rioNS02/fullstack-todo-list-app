93b72d585e4ae8daf2ee86f2498f34b8ce738548
rioNS02 <rionursamsudin242@gmail.com>
2026-01-01 20:38:41 +0700

    Add login api

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -1,5 +1,6 @@
 const database = require("../service/database");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const sendError = require("../helper/sendError");
 const sendSuccess = require("../helper/sendSuccess");
@@ -37,6 +38,54 @@ const register = async (req, res) => {
   }
 };
 
+// Login Auth --
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+  } catch (error) {
+    sendError(res, error, 500);
+  }
+  // Check user found or not
+  const sqlEmail = "SELECT * from users WHERE email = ?";
+  const [rows] = await database.execute(sqlEmail, [email]);
+  const user = rows[0];
+  if (!user) {
+    sendError(res, "User not found, ", 404);
+  }
+
+  // Check password
+  const checKPassword = await bcrypt.compare(password, user.password);
+  if (!checKPassword) {
+    return sendError(res, "Incorrect password", 400);
+  }
+
+  // Create jwt token
+  const payload = {
+    userID: user.userID,
+    email: user.email,
+  };
+
+  // create access token
+  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  // create refresh token
+  const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // cookies
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    nameSite: "none",
+  });
+  res.status(200).json({ message: "login success", accessToken, refreshToken });
+};
+
 module.exports = {
   register,
+  login,
 };
