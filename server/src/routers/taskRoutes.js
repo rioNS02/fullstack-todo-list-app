cd5de043bc980c0efc27f48c467e27fae65c10f7
rioNS02 <rionursamsudin242@gmail.com>
2026-01-03 18:34:55 +0700

    Change middleware

--- server/src/routers/taskRoutes.js
+++ server/src/routers/taskRoutes.js
@@ -6,17 +6,20 @@ const {
   deleteTask,
   updateTask,
   getTaskByUser,
+  updateStatusTask,
 } = require("../controller/taskController");
-const authMiddleware = require("../middleware/authMiddleware");
+const { authAccess } = require("../middleware/authMiddleware");
 
 router.get("/api/v1/tasks", getAllTask);
 
-router.get("/api/v1/tasks/me", authMiddleware, getTaskByUser);
+router.get("/api/v1/tasks/me", authAccess, getTaskByUser);
 
-router.post("/api/v1/tasks", authMiddleware, addTask);
+router.post("/api/v1/tasks", authAccess, addTask);
 
-router.delete("/api/v1/tasks/:id", authMiddleware, deleteTask);
+router.delete("/api/v1/tasks/:id", authAccess, deleteTask);
 
-router.put("/api/v1/tasks/:id", authMiddleware, updateTask);
+router.patch("/api/v1/tasks/:id", authAccess, updateTask);
+
+router.patch("/api/v1/tasks/:id/status", authAccess, updateStatusTask);
 
 module.exports = router;
